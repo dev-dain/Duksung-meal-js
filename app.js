990c8231f0de3716360b299c61ff1ffdc9f306ce
Dain Kim <dev.dain.k@gmail.com>
2020-08-26 20:08:50 +0900

    template을 만들어 코드 양 줄이기

--- app.js
+++ app.js
@@ -8,6 +8,40 @@ fs.readFile('./meal.json', 'utf8', (err, data) => {
   meal = JSON.parse(data);
 });
 
+function getTemplate(simpleText, replyCount=0, reply='') {
+  const quickReplies = [];
+  
+  if (replyCount !== 0) {
+    reply.forEach(value => {
+      quickReplies.push({
+        'label': value,
+        'action': 'message',
+        'messageText': value
+      });
+    });
+  } else {
+    quickReplies.push({
+      'label': '처음으로',
+      'action': 'message',
+      'messageText': '처음으로'
+    });
+  }
+
+  const data = {
+    'version': '2.0',
+    'template': {
+      'outputs': [{
+        'simpleText': {
+          'text': simpleText
+        }
+      }],
+      'quickReplies': quickReplies
+    }
+  }
+  
+  return data;
+}
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
@@ -23,167 +57,42 @@ app.post('/message', (req, res) => {
 
   const question = req.body.userRequest.utterance;
   const goMain = '처음으로';
-  const goBack = '뒤로가기';
+  const goBack = '뒤로 가기';
   const selectDay = '요일지정';
   let data;
 
   if (question === '오늘') {
     if (yoil === 'SUN' || yoil === 'SAT') {
-      data = {
-        'version': '2.0',
-        'template': {
-          'outputs': [{
-            'simpleText': {
-              'text': '오늘은 주말입니다. :)'
-            }
-          }],
-          'quickReplies': [{
-            'label': goMain,
-            'action': 'message',
-            'messageText': goMain
-          }]
-        }
-      }
+      data = getTemplate('오늘은 주말입니다. :)');
     } else {
-      data = {
-        'version': '2.0',
-        'template': {
-          'outputs': [{
-            'simpleText': {
-              'text': `${meal[today].day} ${meal[today].student} ${meal[today].staff}`
-            }
-          }],
-          'quickReplies': [{
-            'label': goMain,
-            'action': 'message',
-            'messageText': goMain
-          }]
-        }
-      }
+      data = getTemplate( `${meal[today].day} ${meal[today].student} ${meal[today].staff}`);
     }
+
   } else if (question === '내일') {
     if (yoil === 'FRI' || yoil === 'SAT') {
-      data = {
-        'version': '2.0',
-        'template': {
-          'outputs': [{
-            'simpleText': {
-              'text': '내일은 주말입니다. :)'
-            }
-          }],
-          'quickReplies': [{
-            'label': goMain,
-            'action': 'message',
-            'messageText': goMain
-          }]
-        }
-      }
+      data = getTemplate('내일은 주말입니다. :)');
     } else {
-      data = {
-        'version': '2.0',
-        'template': {
-          'outputs': [{
-            'simpleText': {
-              'text': `${meal[today + 1].day} ${meal[today + 1].student} ${meal[today + 1].staff}`
-            }
-          }],
-          'quickReplies': [{
-            'label': goMain,
-            'action': 'message',
-            'messageText': goMain
-          }]
-        }
-      }
+      data = getTemplate(`${meal[today + 1].day} ${meal[today + 1].student} ${meal[today + 1].staff}`);
     }
+
   } else if (question === selectDay || question === goBack) {
-    data = {
-      'version': '2.0',
-      'template': {
-        'outputs': [{
-          'simpleText': {
-            'text': '요일을 선택하세요.'
-          }
-        }],
-        'quickReplies': [{
-          'label': '월',
-          'action': 'message',
-          'messageText': '월'
-        }, {
-          'label': '화',
-          'action': 'message',
-          'messageText': '화'
-        }, {
-          'label': '수',
-          'action': 'message',
-          'messageText': '수'
-        }, {
-          'label': '목',
-          'action': 'message',
-          'messageText': '목'
-        }, {
-          'label': '금',
-          'action': 'message',
-          'messageText': '금'
-        }]
-      }
-    }
+    data = getTemplate('요일을 선택하세요.', 5, ['월', '화', '수', '목', '금']);
+
   } else if (question === '상시메뉴') {
-    data = {
-      'version': '2.0',
-      'template': {
-        'outputs': [{
-          'simpleText': {
-            'text': `${meal[0].meal}`
-          }
-        }],
-        'quickReplies': [{
-          'label': goMain,
-          'action': 'message',
-          'messageText': goMain
-        }]
-      }
-    }
+    data = getTemplate(`${meal[0].meal}`);
+
   } else if (question === '월' || question === '화' || question === '수' ||
             question === '목' || question === '금') {
-    const dayObj = { '월': 1, '화': 2, '수': 3, '목': 4, '금': 5 };
-    data = {
-      'version': '2.0',
-      'template': {
-        'outputs': [{
-          'simpleText': {
-            'text': `${meal[dayObj[question]].day} ${meal[dayObj[question]].student} ${meal[dayObj[question]].staff}`
-          }
-        }],
-        'quickReplies': [{
-          'label': goBack,
-          'action': 'message',
-          'messageText': goBack
-        }, {
-	  'label': goMain,
-	  'action': 'message',
-	  'messageText': goMain
-	}]
-      }
-    }
+    const dayObj = { '월': 1, '화': 2, '수': 3, '목': 4, '금': 5};
+    data = getTemplate(`${meal[dayObj[question]].day} ${meal[dayObj[question]].student} ${meal[dayObj[question]].staff}`,
+                        replyCount = 2, reply = [goBack, goMain]);
+
   } else {
-    data = {
-      'version': '2.0',
-      'template': {
-        'outputs': [{
-          'simpleText': {
-            'text': '개발 중이거나 오류입니다.\n' +
-                    '개발자에게 문의해 주세요. \n' +
-                    '이메일 : dev.dain.k@gmail.com'
-          }
-        }],
-        'quickReplies': [{
-          'label': goMain,
-          'action': 'message',
-          'messageText': goMain
-        }]
-      }
-    }
+    data = getTemplate('개발 중이거나 오류입니다.\n' +
+                        '개발자에게 문의해 주세요. \n' +
+                        '이메일 : dev.dain.k@gmail.com');
   } 
+  
   res.json(data);
 });
 
